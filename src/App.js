9a9e69d0d2f224bf257069c51f748e124070b729
Jay Milledge <jm@jay-media.de>
2022-06-23 21:21:33 +0200

    Add movie posters to list, rendering posters

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const API_KEY = process.env.REACT_APP_API_KEY; ////// check back!
   const url = 'https://api.themoviedb.org/3/movie/';
 
+  const posterUrl = 'https://image.tmdb.org/t/p/500/';
+
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
@@ -24,7 +26,7 @@ function App() {
       <div className="movie__container">
         {moviesData.map(movie => (
           <div className="movie__single" key={movie.id}>
-            Image here
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" />
             <h3 className="movie__title">{movie.title}</h3>
           </div>
         ))}
