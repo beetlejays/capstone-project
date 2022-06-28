a7d3f9e6e98013448b9b5d8fc0af8fa772a5d1bf
Jay Milledge <jm@jay-media.de>
2022-06-28 10:52:56 +0200

    Implment movie poster in storybook

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,14 +1,51 @@
+import styled from 'styled-components';
+
 export default function Movie({movie_id, movie_vote_average, movie_poster, movie_title, movie_alt_text}) {
   return (
-    <div className="movie__single" key={movie_id}>
-      <div className="movie__average">
-        <span className="movie__average__number">{movie_vote_average}</span>
-      </div>
+    <MovieSingle key={movie_id}>
+      <MovieAverage>
+        <MovieAverageNumber>{movie_vote_average}</MovieAverageNumber>
+      </MovieAverage>
       <img src={`https://image.tmdb.org/t/p/w500/${movie_poster}`} alt={`${movie_alt_text}`} />
 
       <div>
-        <h3 className="movie__title">{movie_title}</h3>
+        <MovieTitle>{movie_title}</MovieTitle>
       </div>
-    </div>
+    </MovieSingle>
   );
 }
+
+const MovieSingle = styled.div`
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+`;
+
+const MovieAverage = styled.div`
+  position: relative;
+`;
+
+const MovieAverageNumber = styled.div`
+  color: white;
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  font-size: 1.3rem;
+  font-weight: 600;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  background-color: #3083dc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 2px 5px 5px grey;
+`;
+
+const MovieTitle = styled.h3`
+color: white;
+padding: 0.8rem 0;
+font-size: 1rem;
+}
+`;
---
+++ src/components/Movie/Movie.stories.js
@@ -0,0 +1,8 @@
+import Movie from './Movie';
+
+export default {
+  title: 'Components/Movie',
+  component: Movie,
+};
+
+export const Default = () => <Movie />;
