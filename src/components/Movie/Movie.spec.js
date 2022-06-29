82eabd089a414d74fad892b4ec736b75184a1124
Jay Milledge <jm@jay-media.de>
2022-06-29 15:10:25 +0200

    Add Test for title and movie poster

--- src/components/Movie/Movie.spec.js
+++ src/components/Movie/Movie.spec.js
@@ -1,9 +1,14 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 
 import Movie from './Movie';
 
 describe('Movie', () => {
-  it('Shows that the movie component is renderd', () => {
+  it('Shows that the movie poster is being renderd', () => {
     render(<Movie />);
+    screen.getByRole('img');
+  });
+  it('Shows that the movie title is being renderd', () => {
+    render(<Movie />);
+    screen.getByRole('heading');
   });
 });
