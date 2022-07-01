c750714f0345501aef77a4f61d8d5e3d17a4b45a
Jay Milledge <jm@jay-media.de>
2022-07-01 10:38:01 +0200

    Add Link to details

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 import FetchError from '../components/FetchError/FetchError';
@@ -42,21 +43,23 @@ export default function Home() {
         <h2>All popular movies from {yearDate}</h2>
       </IndexHeadingContainer>
 
-      <div className="movie__container">
-        {error && <FetchError />}
+      <Link to="/movie-details">
+        <div className="movie__container">
+          {error && <FetchError />}
 
-        {moviesData.length &&
-          moviesData.map(movie => (
-            <Movie
-              key={movie.id}
-              movie_id={movie.id}
-              movie_vote_average={movie.vote_average}
-              movie_title={movie.title}
-              movie_poster={movie.poster_path}
-              movie_alt_text={movie.original_title}
-            />
-          ))}
-      </div>
+          {moviesData.length &&
+            moviesData.map(movie => (
+              <Movie
+                key={movie.id}
+                movie_id={movie.id}
+                movie_vote_average={movie.vote_average}
+                movie_title={movie.title}
+                movie_poster={movie.poster_path}
+                movie_alt_text={movie.original_title}
+              />
+            ))}
+        </div>
+      </Link>
     </main>
   );
 }
