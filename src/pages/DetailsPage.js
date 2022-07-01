f0641779b70bd5618a315dccced850425c4425b9
Jay Milledge <jm@jay-media.de>
2022-07-01 12:57:30 +0200

    Add back button with functionality

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,40 +1,44 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
 
+import backButton from '../assets/blue-arrow.svg';
 import Header from '../components/Header/Header';
 
 export default function DetailsPage() {
   const params = useParams();
+  const navigate = useNavigate();
 
   return (
     <>
-      <StyledDetailsPage>
-        <main>
-          <section>
-            <MovieDetailPoster></MovieDetailPoster>
-            <div>
-              <h2>{params.title}</h2>
-              <div>
-                <p>{params.vote_average}</p>
-                <img src={`${params.poster_path}`} alt="" />
-                <p>{params.original_title}</p>
-              </div>
-            </div>
-          </section>
-        </main>
-      </StyledDetailsPage>
+      <MovieDetailPoster>
+        <p>Movie Poster here</p>
+      </MovieDetailPoster>
+      <Header />
+      <button onClick={() => navigate(-1)}>
+        <img src={backButton} alt="" />
+      </button>
+      <main>
+        <StyledDetailsPage>
+          <h2>{params.title}</h2>
+          <div>
+            <p>{params.vote_average}</p>
+            <img src={`${params.poster_path}`} alt="" />
+            <p>{params.overview}</p>
+          </div>
+        </StyledDetailsPage>
+      </main>
     </>
   );
 }
 
 const StyledDetailsPage = styled.div`
   max-width: 800px;
-  margin: auto;
+  margin: 100px auto 0 auto;
+  padding: 0 10px;
 `;
 
 const MovieDetailPoster = styled.div`
-  img {
-    width: 100%;
-    height: auto;
-  }
+  width: 100%;
+  height: 350px;
+  background-color: lightgrey;
 `;
