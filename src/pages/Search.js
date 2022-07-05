06a1c9270dff5f56daf96260d789b41b5cb69a88
Jay Milledge <jm@jay-media.de>
2022-07-05 16:26:11 +0200

    Update search page, input

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
@@ -5,7 +7,29 @@ export default function Search() {
   return (
     <>
       <Header />
+      <SearchContainer>
+        <h1>Please type in your movie Search</h1>
+        <SearchInput type="text" />
+      </SearchContainer>
+
       <Footer />
     </>
   );
 }
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 45vh;
+`;
+
+const SearchInput = styled.input`
+  margin-top: 2rem;
+  padding-top: 2rem;
+  border-radius: 40px;
+  width: 80vw;
+  background-color: #666;
+  border: none;
+`;
