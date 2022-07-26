2e73a3147ab88c18e726a135ca2d714021622fee
Jay Milledge <jm@jay-media.de>
2022-07-26 12:20:35 +0200

    Update storybook args

--- src/components/WatchListButton/WatchListButton.stories.js
+++ src/components/WatchListButton/WatchListButton.stories.js
@@ -3,6 +3,13 @@ import WatchListButton from './WatchListButton';
 export default {
   title: 'Components/WatchListButton',
   component: WatchListButton,
+  argTypes: {},
 };
 
-export const Default = () => <WatchListButton />;
+export const Template = args => <WatchListButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  buttonText: 'add to Watchlist +',
+  backgroundColor: 'orange',
+};
